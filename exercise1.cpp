4bc9e912263fe3e0091f3cd3ed83b435755ef666
Beka_Kavlelashvili <beqakavlela2001@gmail.com>
2023-04-30 14:59:36 +0400

    sum of two integers

---
+++ exercise1.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+//მოცემულია ორი მთელი რიცხვი a და b. იპოვეთ მათი ჯამი.
+
+int main(){
+    int a, b;
+    cin >> a >> b;
+
+    cout << a + b;
+
+    return 0;
+}
