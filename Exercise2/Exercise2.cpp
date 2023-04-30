c56a0b7e66380380c6a69bd9b303de3bd3f6b273
Beka_Kavlelashvili <beqakavlela2001@gmail.com>
2023-04-30 15:09:12 +0400

    multiply two integers

---
+++ Exercise2/Exercise2.cpp
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+//მოცემულია ორი მთელი რიცხვი a და b. გამოიტანეთ მათი ნამრავლი.
+int main(){
+    int a, b;
+    cin >> a >> b;
+
+    cout << a * b;
+
+    return 0;
+}
