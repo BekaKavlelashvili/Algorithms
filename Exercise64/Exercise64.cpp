d0092e0ad7c06a53aa666bb9a23b019422f4ff99
Beka_Kavlelashvili <beqakavlela2001@gmail.com>
2023-06-07 18:37:47 +0400

    uppercase symbol (symbols and strings)

---
+++ Exercise64/Exercise64.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+/*
+მოცემულია ლათინური ანბანის პატარა სიმბოლო. საჭიროა მისი გამთავრულება.
+*/
+
+int main() {
+    char sym;
+    cin >> sym;
+
+    sym -= 32;
+
+    cout << (char)sym;
+    return 0;
+}
+
